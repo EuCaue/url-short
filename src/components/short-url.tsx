4bf6173e7f2278707642922945fee28daad79ad6
EuCaue <caueadriano.castro@gmail.com>
2024-08-20 23:09:53 -0300

    feat(short-url): add timer to remove latestShortUrl

--- src/components/short-url.tsx
+++ src/components/short-url.tsx
@@ -1,15 +1,32 @@
 "use client";
 
-import { useLatestShortUrlStore } from "@/stores/useShortUrlsStore";
 import ShortUrlForm from "@/components/short-url-form";
 import UrlCard from "@/components/url-card";
+import { useLatestShortUrlStore } from "@/stores/useShortUrlsStore";
+import { useEffect } from "react";
 
 export default function ShortUrl() {
   const latestShortUrl = useLatestShortUrlStore((state) => state);
+
+  useEffect(() => {
+    if (latestShortUrl.showLatestShortUrl) {
+      const timer = setTimeout(() => {
+        latestShortUrl.setShowLatestShortUrl(false);
+      }, 10000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [latestShortUrl.showLatestShortUrl]);
+
   return (
-    <div>
-    <ShortUrlForm />
-      {latestShortUrl.showLatestShortUrl ? (<UrlCard url={latestShortUrl.latestShortUrl!.url} shrtUrl={latestShortUrl.latestShortUrl!.shrtlnk} />) : ""}
-</div>
-  )
-};
+    <div className="space-y-16">
+      <ShortUrlForm />
+      {latestShortUrl.showLatestShortUrl && (
+        <UrlCard
+          url={latestShortUrl.latestShortUrl!.url}
+          shrtUrl={latestShortUrl.latestShortUrl!.shrtlnk}
+        />
+      )}
+    </div>
+  );
+}
