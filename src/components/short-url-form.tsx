d34e62dcc0fd55afffec6e3dbd2aefaa8d833930
EuCaue <caueadriano.castro@gmail.com>
2024-08-20 23:08:29 -0300

    feat(url-form): use isLoading and refactor submit code

--- src/components/short-url-form.tsx
+++ src/components/short-url-form.tsx
@@ -2,65 +2,83 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useShortUrlsStore, useLatestShortUrlStore } from "@/stores/useShortUrlsStore";
+import {
+  useShortUrlsStore,
+  useLatestShortUrlStore,
+} from "@/stores/useShortUrlsStore";
 import usePersistStore from "@/stores/useStore";
 import { SavedUrl } from "@/types";
+import { Loader } from "lucide-react";
 import { FormEvent, useState } from "react";
 
 export default function ShortUrlForm() {
   const [urlValue, setUrlValue] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const savedUrls = usePersistStore(useShortUrlsStore, (state) => state);
   const latestShortUrl = useLatestShortUrlStore((state) => state);
 
   async function handleSubmit(ev: FormEvent<HTMLFormElement>) {
     ev.preventDefault();
 
+    if (isLoading){
+      return;
+    }
+
+    setIsLoading(true);
+
     const body = { url: urlValue };
 
     try {
-      const response = await fetch('/api/shorten', {
-        method: 'POST',
+      const response = await fetch("/api/shorten", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
+          "Content-Type": "application/json",
+          Accept: "application/json",
         },
         body: JSON.stringify(body),
       });
 
       if (!response.ok) {
-        throw new Error('Failed to shorten the URL');
+        throw new Error("Failed to shorten the URL");
       }
 
       const data = await response.json();
       const { url, key, shrtlnk, message } = data;
       let shortUrls = savedUrls?.urls ?? [];
 
+      console.log(url, key, shrtlnk, message);
+
+      const shortUrl: SavedUrl = { url, key, shrtlnk };
+      const index = shortUrls.findIndex((u) => u.key === key);
+
       if (message) {
-        const index = shortUrls.findIndex((u) => u.key === key);
+        console.debug("here", message);
         if (index !== -1) {
           const [existingUrl] = shortUrls.splice(index, 1);
           shortUrls = [existingUrl, ...shortUrls];
+        } else {
+          shortUrls.push(shortUrl);
         }
       } else {
-        const shortUrl: SavedUrl = { url, key, shrtlnk };
         shortUrls.push(shortUrl);
       }
 
       savedUrls?.setUrls(shortUrls);
-      latestShortUrl.setLatestShortUrl({ url, key, shrtlnk });
-      latestShortUrl.setShowLatestShortUrl();
+      latestShortUrl.setLatestShortUrl(shortUrl);
+      latestShortUrl.setShowLatestShortUrl(true);
       setUrlValue("");
     } catch (error) {
-      console.error('Error shortening the URL:', error);
+      console.error("Error shortening the URL:", error);
+    } finally {
+      setIsLoading(false);
     }
   }
-
 
   return (
     <form onSubmit={handleSubmit}>
       <span className="flex w-full max-w-sm items-center space-x-2">
         <Input
-          className="font-mono p-2 placeholder:font-bold"
+          className="font-mono p-2 placeholder:font-bold shadow-md"
           title="Insert a URL to be shortened here."
           aria-label="Insert a URL to be shortened here."
           type="url"
@@ -73,7 +91,11 @@ export default function ShortUrlForm() {
           title="Press the button, to shorten the URL"
           aria-label="Press the button, to shorten the URL"
         >
-          Short!
+          {isLoading ? (
+            <Loader size={16} className="animate-spin text-muted" />
+          ) : (
+            "short it"
+          )}
         </Button>
       </span>
     </form>
